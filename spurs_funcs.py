fd6815dbba575a5abef1c3aadaf167079ee5b573
Lauren Cadle <laurencadle@Laurens-MacBook-Pro-3.local>
2026-07-29 14:53:07 -0700

    update spurs_funcs.py

--- spurs_funcs.py
+++ spurs_funcs.py
@@ -1,10 +1,9 @@
 import pandas as pd 
 import scipy.stats as stats
-import os
-import sys
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
+from scipy.stats import norm, cauchy
 
 def get_bounds(line, viewing_window = 30, fitting_window = 5):
     lower_bound = line - viewing_window
@@ -47,7 +46,7 @@ def fit_peak(quadratic_func, line, counts, peak_mask, bin_centers, slope_val, fi
 
 def fit_peak(gaussian_func, line, counts, peak_mask, bin_centers, slope_val, fitted_lower, fitted_upper):
     p0_gauss = [line, 2, slope_val, counts[peak_mask].min(), 10000]
-    popt, pcov = curve_fit(gaussian_func, bin_centers[peak_mask], counts[peak_mask], p0=p0_gauss, bounds=([fitted_lower, 1, 3*slope_val, 0, 0], [fitted_upper, 10, 0, 1e6, np.inf]), maxfev=10000)
+    popt, pcov = curve_fit(gaussian_func, bin_centers[peak_mask], counts[peak_mask], p0=p0_gauss, bounds=([fitted_lower, 1, 3*slope_val, 0, -500], [fitted_upper, 10, 0, 1e6, np.inf]), maxfev=50000)
     p_err= np.sqrt(np.diag(pcov))
     
     return popt, pcov, p_err
@@ -65,7 +64,8 @@ def plot_gaussian(line, N, p_err, counts, bin_edges, popt, lower_bound, upper_bo
     plt.xlim(lower_bound, upper_bound)
     plt.ylim(*ylim)
     plt.axvspan(fitted_lower, fitted_upper, alpha=0.12, color='grey', label=f'Fit window ({fitted_lower}–{fitted_upper} keV)')
-    plt.errorbar(x=popt[0], y=gaussian_func(popt[0], *popt), xerr=p_err[0], fmt='o', color='red', label=f'Peak error: ±{p_err[0]:.2f} keV')
+    plt.axvline(x=popt[0] - p_err[0], color='red', linestyle='--', label=f'Peak error: ±{p_err[0]:.2f} keV')
+    plt.axvline(x=popt[0] + p_err[0], color='red', linestyle='--')
     plt.legend()
     plt.tight_layout()
     plt.show()
@@ -89,3 +89,28 @@ def plot_quadratic(line, N, p_err, counts, bin_edges, popt, lower_bound, upper_b
     plt.tight_layout()
     plt.show()
     return x1, fitted_model_g
+
+def snr(popt_g, counts, bin_centers):
+    mu, sigma, c, d, A = popt_g
+    lower = mu - 1.4*sigma
+    upper = mu + 1.4*sigma
+    
+    window_mask = (bin_centers > lower) & (bin_centers < upper)
+    window_totals = np.sum(counts[window_mask])
+    background_in_window = np.sum(c * bin_centers[window_mask] + d)
+    
+    S = window_totals - background_in_window
+    B = background_in_window
+    
+    return S / np.sqrt(S + B)
+
+
+#def snr(popt_g, counts, bin_width):
+    mu, sigma, c, d, A = popt_g
+    total_counts = np.sum(counts)
+    lower = mu - 1.4*sigma
+    upper = mu + 1.4*sigma
+    h = upper - lower
+    S = (norm.cdf(upper, mu, sigma) - norm.cdf(lower, mu, sigma)) * A * total_counts * bin_width
+    B = 1/2 * ((c*lower + d) + (c*upper + d)) * h
+    return S / np.sqrt(S + B)
